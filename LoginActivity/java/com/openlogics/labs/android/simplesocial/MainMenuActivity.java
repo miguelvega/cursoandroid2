eaa16a12a7afab5349f9b80a38d63a39bfb5a538
Miguel Vega <mikevegap@gmail.com>
2013-06-05 19:03:01 -0400

    more apps

---
+++ LoginActivity/java/com/openlogics/labs/android/simplesocial/MainMenuActivity.java
@@ -0,0 +1,15 @@
+package com.openlogics.labs.android.simplesocial;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author Miguel Vega
+ * @version $Id: MainMenuActivity.java 0, 2013-05-21 1:14 AM mvega $
+ */
+public class MainMenuActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainmenu);
+    }
+}
